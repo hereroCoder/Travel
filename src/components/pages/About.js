a8c04e0e3e1059326f15647b8b9b6b7aee28fbdc
ElenaGospodinova <gospodinovaelena@yahoo.co.uk>
2023-05-12 17:51:09 +0100

    style gallery

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,53 +4,54 @@ import wave from "../img/wave.jpeg";
 
 function About() {
   return (
-    <section className="team-page-section">
-      <div className="container">
-        <div className="sec-title centered">
-          <div className="title">Our Team</div>
-          <div className="separator"><span></span></div>
-          <h2>Elena </h2>
-        </div>
+    <></>
+    // <section className="team-page-section">
+    //   <div className="container">
+    //     <div className="sec-title centered">
+    //       <div className="title">Our Team</div>
+    //       <div className="separator"><span></span></div>
+    //       <h2>Elena </h2>
+    //     </div>
 
-        <div className="row clearfix">
-          <div className="team-block col-lg-4 col-md-8 col-sm-12">
-            <div
-              className="inner-box wow fadeInLeft"
-              data-wow-delay="0ms"
-              data-wow-duration="1500ms"
-            >
-              <ul className="social-icons">
-                <li>
-                  <a href="#"><FaFacebook /></a>
-                </li>
-                <li>
-                  <a href="#"><FaTwitter /></a>
-                </li>
-                <li>
-                  <a href="#"><FaInstagram /></a>
-                </li>
-                <li>
-                  <a href="#"><FaLinkedinIn /></a>
-                </li>
-              </ul>
-              <div className="image">
-                <a href="#">
-                  <img src={wave} alt="" />
-                </a>
-              </div>
-              <div className="lower-content">
-                <h3>
-                  <a href="#">Info/About</a>
-                </h3>
-                <div className="designation">Hi there :) </div>
-              </div>
-            </div>
-          </div>
+    //     <div className="row clearfix">
+    //       <div className="team-block col-lg-4 col-md-8 col-sm-12">
+    //         <div
+    //           className="inner-box wow fadeInLeft"
+    //           data-wow-delay="0ms"
+    //           data-wow-duration="1500ms"
+    //         >
+    //           <ul className="social-icons">
+    //             <li>
+    //               <a href="#"><FaFacebook /></a>
+    //             </li>
+    //             <li>
+    //               <a href="#"><FaTwitter /></a>
+    //             </li>
+    //             <li>
+    //               <a href="#"><FaInstagram /></a>
+    //             </li>
+    //             <li>
+    //               <a href="#"><FaLinkedinIn /></a>
+    //             </li>
+    //           </ul>
+    //           <div className="image">
+    //             <a href="#">
+    //               <img src={wave} alt="" />
+    //             </a>
+    //           </div>
+    //           <div className="lower-content">
+    //             <h3>
+    //               <a href="#">Info/About</a>
+    //             </h3>
+    //             <div className="designation">Hi there :) </div>
+    //           </div>
+    //         </div>
+    //       </div>
 
-          {/* Repeat the above structure for the remaining team blocks */}
-        </div>
-      </div>
-    </section>
+    //       {/* Repeat the above structure for the remaining team blocks */}
+    //     </div>
+    //   </div>
+    // </section>
   );
 }
 
--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -25,7 +25,7 @@ export default function Navbar() {
 					<FontAwesomeIcon icon={faImage} size='sm' />
 					&nbsp; Gallery
 				</CustomLink>
-				<CustomLink to='/experience'>
+				<CustomLink to='/about'>
 					<FontAwesomeIcon icon={faUserGroup} size='sm' />
 					&nbsp;About US
 				</CustomLink>
